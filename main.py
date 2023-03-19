49a58adffbca0c5cdf0a2c79826d27d1744b4a67
Tree <keithmaccuish5@gmail.com>
2023-03-18 20:40:12 -0600

    empty project

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from Treengine.Game.TreeGame import TreeGame
 
-from game.scenes.MenuScene.MainMenu import MainMenu
-from game.scenes.PlayScene.PlayScene import PlayScene
-from game.scenes.EditorScene.EditorScene import EditorScene
-
 
 def main():
     game = TreeGame()
 
-    game.scenes = [
-        MainMenu(game, "main_scene"),
-        PlayScene(game, "game_scene"),
-        EditorScene(game, "level_editor"),
-    ]
-
-    game.current_scene = game.scenes[0]
 
     game.run()
 
